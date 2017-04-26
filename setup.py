e716a6fcc315923a02b6b536cd39c71f18c32732
ScriptSmith <ScriptSmith@users.noreply.github.com>
2017-04-26 12:43:55 +1000

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='socialreaper',
-    version='0.2.2',
+    version='0.3.0',
     packages=['socialreaper'],
     package_dir={'socialreaper': 'socialreaper'},
     install_requires=['requests>=2.11.1', 'requests-oauthlib>=0.7.0',
